f28988bb10cb3f8ad533da77a6dbd9639c127a25
Aaron Zhu <zghcivilization@163.com>
2021-12-26 19:56:27 +0800

    1. 修改Map示例 2. 添加Range示例

--- src/main/groovy/com/aaron/BasicDemo/MapDemo.groovy
+++ src/main/groovy/com/aaron/BasicDemo/MapDemo.groovy
@@ -6,6 +6,7 @@ package com.aaron.BasicDemo
 class MapDemo {
     static void main(String[] args) {
         basic()
+        testType()
     }
 
     static void basic() {
@@ -27,16 +28,34 @@ class MapDemo {
         map1."Tim" = 7
         assert map1 == ["Aaron":2, "Bob":1, "Tony":35, "Tim":7, "Tom":996]
 
+        // 迭代Map中的条目按闭包进行计算，并将结果保存到列表中
+        def list1 = map1.collect{ entry -> entry.value+1000 }
+        assert list1 instanceof List
+        println("list1: $list1")
+
+        def list2 = []
+        // 将结果保存到给定的List中
+        map1.collect(list2) { entry -> entry.value+2000}
+        assert list1 instanceof List
+        println("list2: $list2")
+
         // Map中每一个KV对是否均满足闭包条件
         assert map1.every {entry -> entry.value>0}
         assert !map1.every {entry -> entry.value>3}
         // Map中是否存在一个KV对满足闭包条件
         assert map1.any {entry -> entry.key=="Aaron"}
 
-        def result1 =  map1.find { entry -> entry.value>5 }
+        def map5 = ["Bob":3, "Aaron":18, "Tom": 23]
+        // 查找任一一个满足闭包条件的条目
+        def entry1 =  map5.find { entry -> entry.value>5 }
         // 占位符完整语法为 ${}, 在不引起歧义的情况下可以直接使用 $
-        println ("result 1: $result1.key --->>> ${result1.value}")
+        println ("result 1: $entry1.key --->>> ${entry1.value}")
+        // 查找所有满足闭包条件的条目
+        def resutMap =  map5.findAll { entry -> entry.value>5 }
+        println ("resutMap : $resutMap")
+    }
 
+    static void testType() {
         /********************** 类型指定 **********************/
         // 定义一个空Map, 通过as运算符强制类型为TreeMap
         def map2 = [:] as TreeMap
@@ -50,5 +69,6 @@ class MapDemo {
         TreeMap map4 = ["Aaron":25, "Bob":18, "Tony": 35]
         assert map4 instanceof TreeMap
     }
+
 
 }
---
+++ src/main/groovy/com/aaron/BasicDemo/RangeDemo.groovy
@@ -0,0 +1,40 @@
+package com.aaron.BasicDemo
+
+/**
+ * Groovy Range 示例
+ */
+class RangeDemo {
+    static void main(args) {
+        // range1: [0, 5]
+        def range1 = 0..5
+        assert range1 instanceof Range
+        // contains: 检测某个元素是否存在于范围中
+        assert range1.contains(0)
+        assert range1.contains(3)
+        assert range1.contains(5)
+        assert range1.size() == 6
+
+        def sum = 0
+        // each: 对范围中每个元素执行闭包
+        range1.each { e -> sum += e }
+        // sum = 0+1+2+3+4+5 = 15
+        assert sum == 15
+
+        // range2: [0, 5)
+        def range2 = 0..<5
+        assert range2.contains(5) == false
+
+        // 日期范围
+        def today = new Date()
+        def yesterday = today - 1
+        assert (yesterday..today).size() == 2
+
+        // 字符范围
+        def range3 = 'a'..'c'
+        assert range3.contains("b")
+
+        // 反向范围
+        def range4 = 4..<1
+        assert range4.toList() == [4, 3, 2]
+    }
+}
