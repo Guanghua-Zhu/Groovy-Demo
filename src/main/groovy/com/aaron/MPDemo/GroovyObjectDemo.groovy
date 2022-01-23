7e1e6852193b7582eeb96b8b5227f88ec02cfb40
Aaron Zhu <zghcivilization@163.com>
2022-01-23 20:34:32 +0800

    1. [Add]: MP: GroovyObject Interface Demo

---
+++ src/main/groovy/com/aaron/MPDemo/GroovyObjectDemo.groovy
@@ -0,0 +1,14 @@
+package com.aaron.MPDemo
+
+/**
+ *  Groovy MP元编程示例: 调用方法缺失
+ * @author Aaron Zhu
+ * @date 2022-01-23
+ */
+class GroovyObjectDemo {
+    static void main(String[] args){
+
+    }
+}
+
+
